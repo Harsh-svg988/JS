c3c138dd61aed34ede969e845fb32995593c198e
Harsh-svg988 <harshprabhakar305@gmail.com>
2024-08-31 00:13:55 +0530

    Time

--- DateTime.js
+++ DateTime.js
@@ -5,4 +5,14 @@ newDate.toLocaleDateString('default',{
     weekday:'long',
 
 })
-console.log(newDate.toLocaleDateString());
+// console.log(newDate.toLocaleDateString());
+
+
+let myCreatedDate = new Date(2024,9,31);//Custom Date
+console.log(myCreatedDate.toLocaleString())
+
+
+// TIME
+let myTimeStamp = Date.now();
+console.log(myTimeStamp);//It prints number of milliseconds from 1 JAN 1970
+console.log(myCreatedDate.getTime())
